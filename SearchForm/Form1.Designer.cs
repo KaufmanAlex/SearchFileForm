2c34b1eb1c9ac4f49ceb4d23cef28bb08997c73f
Kaufman Alex <kau-alex@mail.ru>
2015-02-06 14:28:20 +0400

    Зафиксировать решение

---
+++ SearchForm/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace SearchForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbStartDir = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.lbResults = new System.Windows.Forms.ListBox();
+            this.BrausDlg = new System.Windows.Forms.FolderBrowserDialog();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.tbWord = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.labKolRez = new System.Windows.Forms.Label();
+            this.labTime = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbFile
+            // 
+            this.tbFile.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.tbFile.Location = new System.Drawing.Point(15, 24);
+            this.tbFile.Name = "tbFile";
+            this.tbFile.Size = new System.Drawing.Size(320, 20);
+            this.tbFile.TabIndex = 0;
+            this.tbFile.Text = "*.txt";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Шаблон имени файла:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Стартовая директория:";
+            // 
+            // tbStartDir
+            // 
+            this.tbStartDir.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.tbStartDir.Location = new System.Drawing.Point(12, 139);
+            this.tbStartDir.Name = "tbStartDir";
+            this.tbStartDir.Size = new System.Drawing.Size(239, 20);
+            this.tbStartDir.TabIndex = 3;
+            this.tbStartDir.Text = "D:\\";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(260, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Обзор...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(67, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 30);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Поиск";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(168, 8);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(173, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Очистить результаты";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // lbResults
+            // 
+            this.lbResults.FormattingEnabled = true;
+            this.lbResults.Location = new System.Drawing.Point(7, 35);
+            this.lbResults.Name = "lbResults";
+            this.lbResults.Size = new System.Drawing.Size(464, 368);
+            this.lbResults.TabIndex = 7;
+            this.lbResults.MouseClick += new System.Windows.Forms.MouseEventHandler(this.lbResults_MouseClick);
+            this.lbResults.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.Results_MouseDoubleClick);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(15, 213);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(320, 188);
+            this.treeView1.TabIndex = 9;
+            // 
+            // tbWord
+            // 
+            this.tbWord.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.tbWord.Enabled = false;
+            this.tbWord.Location = new System.Drawing.Point(15, 69);
+            this.tbWord.Name = "tbWord";
+            this.tbWord.Size = new System.Drawing.Size(320, 20);
+            this.tbWord.TabIndex = 11;
+            this.tbWord.Text = "select";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(12, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Текст в файле:";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(0, -2);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.splitContainer1.Panel1.Controls.Add(this.checkBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            this.splitContainer1.Panel1.Controls.Add(this.tbStartDir);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.labKolRez);
+            this.splitContainer1.Panel2.Controls.Add(this.labTime);
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.lbResults);
+            this.splitContainer1.Panel2.Controls.Add(this.button3);
+            this.splitContainer1.Size = new System.Drawing.Size(831, 413);
+            this.splitContainer1.SplitterDistance = 347;
+            this.splitContainer1.TabIndex = 13;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 94);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(132, 17);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Вкл. поиск по тексту";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // labKolRez
+            // 
+            this.labKolRez.AutoSize = true;
+            this.labKolRez.Location = new System.Drawing.Point(84, 13);
+            this.labKolRez.Name = "labKolRez";
+            this.labKolRez.Size = new System.Drawing.Size(13, 13);
+            this.labKolRez.TabIndex = 10;
+            this.labKolRez.Text = "0";
+            // 
+            // labTime
+            // 
+            this.labTime.AutoSize = true;
+            this.labTime.Location = new System.Drawing.Point(424, 13);
+            this.labTime.Name = "labTime";
+            this.labTime.Size = new System.Drawing.Size(34, 13);
+            this.labTime.TabIndex = 9;
+            this.labTime.Text = "00.00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Результатов:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 411);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbWord);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFile);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SearchForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog BrausDlg;
+        public System.Windows.Forms.ListBox lbResults;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label labTime;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labKolRez;
+        public System.Windows.Forms.Timer timer1;
+        public System.Windows.Forms.TextBox tbFile;
+        public System.Windows.Forms.TextBox tbStartDir;
+        public System.Windows.Forms.TextBox tbWord;
+    }
+}
+
---
+++ SearchForm/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using Timer = System.Threading.Timer;
+
+namespace SearchForm
+{
+    public partial class Form1 : Form
+    {
+        public int ex_count; // количество исключений
+
+        Timer tmr1;
+        System.DateTime date1;
+
+        // Конструктор
+        public Form1()
+        {
+            InitializeComponent();
+            
+            // Загрузка введенных данных
+            System.IO.StreamReader textFile = new System.IO.StreamReader("temp666.txt", Encoding.UTF8);
+
+            tbFile.Text = textFile.ReadLine();
+            tbWord.Text = textFile.ReadLine();
+            tbStartDir.Text = textFile.ReadLine();
+            textFile.Close();                                
+        }
+
+        // Функция поиска
+        private void Search(string way, string mask)
+        {
+            string[] Dirs = null;
+            string[] files = null;
+            string cdir = null;
+            try
+            {
+                files = Directory.GetFiles(way, mask);
+
+                foreach (var f in files)
+                {
+                    if (checkBox1.Checked == false)
+                    {
+                        lbResults.Items.Add(f);
+                    }
+                    else
+                    {
+                        var word = File.ReadAllLines(f);
+                        var LogHashSet = new HashSet<string>(word);
+                        if (LogHashSet.Contains(tbWord.Text) == true)
+                            lbResults.Items.Add(f);
+                        else { }
+                    }
+                }
+                Dirs = Directory.GetDirectories(way, "*", SearchOption.TopDirectoryOnly);
+            }
+            catch (Exception)
+            {
+                ex_count++;
+            }
+            if ((Dirs != null))
+            {
+                foreach (string cdir_loopVariable in Dirs)
+                {
+                    cdir = cdir_loopVariable;
+                    Search(cdir, mask);
+                }
+            }
+        }
+
+        // Кнопка "Обзор"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (BrausDlg.ShowDialog() == DialogResult.OK)
+            {
+                tbStartDir.Text = BrausDlg.SelectedPath;
+            }
+        }
+
+        // Кнопка "Поиск"
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                button3_Click(sender, e);
+
+                //   string [] mas = new string [2];
+                //   string [] mas = {Convert.ToString(sender), Convert.ToString(e)};
+
+                timer1.Start();
+                tmr1 = new Timer(Tick1, "tick1...", 0, 1000);
+
+                Search(tbStartDir.Text, tbFile.Text);
+
+                string infostr = "Поиск завершен. Найдено " + lbResults.Items.Count + " файлов." + ". Обработано " + ex_count + " исключений.";
+                labKolRez.Text = Convert.ToString(lbResults.Items.Count);
+                MessageBox.Show(infostr);
+            }
+            catch 
+            {
+                MessageBox.Show("Возникла ошибка", "Error");
+            }
+        }
+
+        private void Tick1(object data)   // выполнение пула потока
+        {
+            date1 = date1.AddSeconds(1);
+        }
+
+        // Кнопка "Очистить все"
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lbResults.Items.Clear();
+            treeView1.Nodes.Clear();
+
+            ex_count = 0;
+            date1 = new DateTime(2015, 1, 27, 0, 0, 0);
+            labTime.Text = "00:00";
+            labKolRez.Text = "0";
+        }
+
+        // Двойной клик мыши по результатам для открытия
+        private void Results_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                Process.Start(lbResults.SelectedItem.ToString());
+            }
+            catch { }
+        }
+
+        // Одиночный клик мыши по результатам для отображения дерева
+        private void lbResults_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                treeView1.Nodes.Clear();
+                string path = lbResults.SelectedItem.ToString();
+                string[] y = path.Split('\\');
+
+                TreeNode[] mas = new TreeNode[y.Count()];
+
+                mas[0] = treeView1.Nodes.Add(y[0]);           
+                for (int i = 1; i < y.Count(); i++)
+                {
+                    mas[i] = mas[i - 1].Nodes.Add(y[i]);
+                }
+                mas[0].ExpandAll();
+            }
+            catch { }
+        }
+
+        // Закрытие формы
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            // Сохранение введенных данных
+            System.IO.StreamWriter textFile = new System.IO.StreamWriter("temp666.txt");
+            textFile.WriteLine(tbFile.Text);
+            textFile.WriteLine(tbWord.Text);
+            textFile.WriteLine(tbStartDir.Text);
+            textFile.Close();                       
+        }
+//-----------------------------------------------------------------------------------------
+        // Чекбокс (интерфейс)
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) tbWord.Enabled = true;
+            else tbWord.Enabled = false;
+        }
+
+        // Таймер
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labTime.Text = date1.ToString("mm:ss");
+
+            timer1.Stop();
+            tmr1.Dispose();
+        }
+    }
+}
